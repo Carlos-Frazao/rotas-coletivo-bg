fbe80f8e66baaffc189f9e3258456eb1497a2211
Carlos-Frazao <ce416669@gmail.com>
2025-09-03 23:45:13 -0300

    Rotas

---
+++ rotas/rota-linha-01.js
@@ -0,0 +1,11 @@
+// Dados da Rota 01 (Terminal -> Abel Lira)
+var rota01 = {
+    nome: "Terminal - Abel Lira",
+    pontos: [
+        [-15.885499798155381, -52.26663203796818]// Coordenadas do Terminal 
+
+        // ... vários outros pontos até o bairro
+
+        [-15.881432548827298, -52.29645425333495] // Coordenadas do Bairro Abel Lira
+    ]
+};
